7505ff6db0308978a8b76667d2495a6e6f7ee5ff
Devin B <devinb@Devins-MacBook-Pro.local>
2024-06-04 14:22:11 -0700

    Add float animation. Update styling. Begin building out theme context.

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -29,14 +29,16 @@ const CardBase = ({
     <StyledCardWrapper>
       <div className="z-10 border border-black/5 bg-white/30 p-10 shadow-sm backdrop-blur-md">
         {header && (
-          <div className="mb-4 border-b-2 border-slate-800 py-2">{header}</div>
+          <div className="mb-4 border-b-2 border-slate-800 py-2.5">
+            {header}
+          </div>
         )}
         {children}
       </div>
       {bgObject === 'left' ? (
-        <div className="absolute -bottom-2 left-2 -z-10 h-56 w-1/5 rounded-lg bg-gradient-to-b from-slate-200 to-slate-400" />
+        <div className="animate-float absolute -bottom-5 -left-3 -z-10 h-72 w-12 rounded-md bg-gradient-to-b from-slate-200 to-slate-400 shadow-md" />
       ) : bgObject === 'right' ? (
-        <div className="absolute -bottom-2 right-2 -z-10 h-56 w-1/5 rounded-lg bg-gradient-to-b from-slate-200 to-slate-400" />
+        <div className="animate-float absolute -bottom-5 -right-3 -z-10 h-72 w-12 rounded-md bg-gradient-to-b from-slate-200 to-slate-400 shadow-md" />
       ) : null}
     </StyledCardWrapper>
   );
