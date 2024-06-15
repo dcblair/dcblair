59917778ced708598ac6c1f87d6b2689937a2f03
Devin B <devinb@Devins-MacBook-Pro.local>
2024-06-14 17:43:27 -0700

    add bullet points to /experience job cards. improve card sizing and transitions. update spinner. adjust fallback styling.

--- app/components/LoadingSpinner/LoadingSpinner.tsx
+++ app/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,11 +2,7 @@ import { memo } from 'react';
 
 const BaseLoadingSpinner = () => {
   return (
-    <div className="w-h-20 relative flex h-20 items-center justify-center md:h-32 md:w-32">
-      <div className="relative h-full w-full animate-spin rounded-full bg-gradient-to-l from-slate-100 to-slate-800" />
-      {/* TODO: Make the center transparent */}
-      <div className="absolute h-16 w-16 rounded-full bg-white md:h-20 md:w-20" />
-    </div>
+    <div className="-bottom-2 -right-20 -z-10 h-40 w-40 animate-floatFast rounded-full bg-gradient-to-b from-slate-100 to-slate-700 shadow-md" />
   );
 };
 
--- app/routes/experience.tsx
+++ app/routes/experience.tsx
@@ -3,7 +3,7 @@ import { IconCanopyServicing, IconComcastBusiness } from '~/components/Icons';
 
 const Experience = () => {
   return (
-    <div className="grow-1 flex h-full w-full flex-col items-center p-6 md:space-y-12">
+    <div className="grow-1 flex h-full w-full flex-col items-center p-2 md:space-y-12 md:p-6">
       <div className="relative w-full md:w-1/3">
         {/* Experience heading */}
         <Card
@@ -14,7 +14,7 @@ const Experience = () => {
           }
         />
 
-        {/* Floating bar */}
+        {/* Ligher floating globe */}
         <div className="invisible absolute -bottom-2 -right-20 -z-10 h-40 w-40 animate-floatSlow rounded-full bg-gradient-to-b from-slate-200 to-slate-600 shadow-md md:visible" />
       </div>
 
@@ -31,11 +31,23 @@ const Experience = () => {
               </h4>
             </div>
 
-            <ul className="space-y-1 rounded-md bg-gradient-to-b from-white/90 to-slate-100/90 p-3 tracking-wider shadow-lg">
-              <li>• did the thing</li>
-              <li>• did the other thing</li>
-              <li>• </li>
-              <li>• </li>
+            <ul className="space-y-1 rounded-md bg-gradient-to-b from-white/90 to-slate-100/90 p-6 text-sm tracking-wider shadow-lg">
+              <li>
+                • Implemented a working solution for MUI accessibility bug—
+                improving accessibility by 7%, triaged and created subsequent QA
+                and engineering tickets with clear and concise descriptions and
+                acceptance criteria
+              </li>
+              <li>
+                • Configured and maintained 4 React and Remix applications
+                across 2 Frontend teams, leading to a significant increase in
+                application stability and decrease in ramp up time for new devs
+              </li>
+              <li>
+                • Developed a reusable d3.js chart component to visualize
+                bandwidth data for customers, enhancing data accessibility and
+                user engagement
+              </li>
             </ul>
           </Card>
 
@@ -45,65 +57,117 @@ const Experience = () => {
 
         {/* Comcast photo */}
         <div className="flex items-center justify-center">
-          <Card
-            className="duration-2000 w-full hover:bg-slate-500/20 hover:shadow-2xl hover:transition-all md:w-96"
-            variant="tertiary"
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            to="https://business.comcast.com/"
+            variant="icon"
           >
-            {/* Improve accessibility */}
-            <Link
-              className="flex w-full items-center justify-center"
-              target="_blank"
-              rel="noreferrer"
-              to="https://business.comcast.com/"
-              variant="icon"
+            <Card
+              className="w-full duration-2000 hover:bg-slate-600/20 hover:shadow-2xl hover:transition-all md:w-96"
+              variant="tertiary"
             >
+              {/* TODO: Improve accessibility */}
               <IconComcastBusiness />
-            </Link>
-          </Card>
+            </Card>
+          </Link>
         </div>
 
         {/* Canopy */}
         <div className="flex items-center">
           <Card>
-            <h3 className="text-xl font-semibold">
-              Canopy Servicing - Software Engineer
-            </h3>
-            <h4 className="text-lg font-medium">July 2021 - July 2022</h4>
+            <div className="mb-4 space-y-1">
+              <h3 className="text-xl font-semibold">
+                Canopy Servicing - Software Engineer
+              </h3>
+              <h4 className="text-lg font-medium">July 2021 - July 2022</h4>
+            </div>
+
+            <ul className="space-y-1 rounded-md bg-gradient-to-t from-white/90 to-slate-50/90 p-6 text-sm tracking-wider shadow-lg">
+              <li>
+                • Led a project to migrate authentication from Auth0 to Cognito,
+                reducing expenditure by over $50,000
+              </li>
+              <li>
+                • Developed a component library using Typescript, native HTML,
+                Tailwind, and Headless UI, enabling granular component
+                customization and enhancing application performance by reducing
+                package bloat
+              </li>
+              <li>
+                • Created robust Backend endpoints using Node.js and seamlessly
+                connected them to the Frontend in Next.js, enabling efficient
+                data retrieval and interaction
+              </li>
+              <li>
+                • Mentored intern, junior, and mid-level engineers through
+                pairing sessions, one-on-one career and personal development
+                counseling, and knowledge transfer meetings, resulting in an
+                increase in team productivity and skill advancement
+              </li>
+            </ul>
           </Card>
         </div>
 
         {/* Canopy photo */}
         <div className="flex items-center justify-center">
-          <Card
-            className="duration-2000 w-full hover:bg-slate-500/20 hover:shadow-2xl hover:transition-all md:w-96"
-            variant="tertiary"
+          <Link
+            rel="noreferrer"
+            target="_blank"
+            to="https://www.canopyservicing.com/canopyos"
           >
-            <Link
-              className="w-full"
-              rel="noreferrer"
-              target="_blank"
-              to="https://www.canopyservicing.com/canopyos"
+            <Card
+              className="w-full duration-2000 hover:bg-slate-600/20 hover:shadow-2xl hover:transition-all md:w-96"
+              variant="tertiary"
             >
               <IconCanopyServicing />
-            </Link>
-          </Card>
-          <div className="absolute -z-10 h-8 w-8 -rotate-12 transform rounded-full bg-gradient-to-br from-slate-200 to-black bg-fixed md:left-72 md:h-full md:w-2" />
+            </Card>
+          </Link>
         </div>
 
         {/* Zeal */}
         <div className="relative flex items-center">
           <Card>
-            <h3 className="text-xl font-semibold">
-              Zeal Enterprises - Lead Frontend Engineer
-            </h3>
-            <h4 className="text-lg font-medium">January 2021 - July 2021</h4>
+            <div className="mb-4 space-y-1">
+              <h3 className="text-xl font-semibold">
+                Zeal Enterprises - Lead Frontend Engineer
+              </h3>
+              <h4 className="text-lg font-medium">January 2021 - July 2021</h4>
+            </div>
+
+            <ul className="space-y-1 rounded-md bg-gradient-to-b from-white/90 to-slate-100/85 p-6 text-sm tracking-wider shadow-lg">
+              <li>
+                • Built out frontend architecture, learning and implementing new
+                technologies in fast-paced sprints
+              </li>
+              <li>
+                • Researched and designed a migration plan to Typescript,
+                resulting in refactoring of the codebase to adopt type-safe,
+                better-documented code
+              </li>
+              <li>
+                • Worked with the engineering team and UX design team in agile,
+                sprint-based development, engaging in mentorship and pair
+                programming to strengthen team synergy and growth
+              </li>
+              <li>
+                • Proposed and implemented code splitting to improve loading
+                latency by 10%
+              </li>
+              <li>• Configured Webpack for optimized bundling</li>
+            </ul>
           </Card>
-          <div className="absolute -z-10 h-8 w-8 animate-floatSlow rounded-full bg-gradient-to-br from-slate-200 to-black bg-fixed motion-reduce:animate-none md:-left-20 md:top-8 md:h-28 md:w-28" />
+
+          {/* Another floating globe */}
+          <div className="absolute -z-10 h-8 w-8 animate-floatSlow rounded-full bg-gradient-to-br from-slate-200 to-black bg-fixed motion-reduce:animate-none md:-right-20 md:top-2 md:h-28 md:w-28" />
         </div>
 
         {/* Zeal photo */}
         <div className="flex items-center justify-center">
-          <Card className="w-full" variant="tertiary">
+          <Card
+            className="duration-2000 hover:shadow-2xl hover:transition-all md:w-96"
+            variant="tertiary"
+          >
             <img
               className="h-32 w-32"
               src="assets/zeal_logo.jpg"
