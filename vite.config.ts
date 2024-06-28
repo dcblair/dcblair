832133e5a16958c01af1f66ac4f8ba6b6016db6b
Devin B <devinb@Devins-MacBook-Pro.local>
2024-06-27 17:58:41 -0700

    minify code.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  build: {
+    cssMinify: true,
+    minify: true,
+  },
   plugins: [
     remix({
       ssr: false,
