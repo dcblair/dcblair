4226a75698b981d6f2e7fd1f8eac6d14bfee3d2c
Devin B <devin.c.blair@gmail.com>
2024-10-15 12:00:50 -0700

    update yaml.

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,6 @@
 import { vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig, Plugin } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-function transformPdfJsWorker(): Plugin {
-  return {
-    name: 'transform-pdf-js-worker',
-    generateBundle(options: any, bundle: any) {
-      for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
-        if (
-          !fileName.includes('pdf.worker') ||
-          (chunkOrAsset as any).type !== 'asset'
-        ) {
-          continue;
-        }
-        const prepend = Buffer.from(
-          `if (typeof Promise.withResolvers === "undefined") {
-            Promise.withResolvers = function () {
-              let resolve, reject
-              const promise = new Promise((res, rej) => {
-                resolve = res
-                reject = rej
-              })
-              return { promise, resolve, reject }
-            }
-          }
-          `,
-          'utf-8',
-        );
-        const source = (chunkOrAsset as { source: string | Buffer }).source;
-        const sourceBuffer = Buffer.isBuffer(source)
-          ? source
-          : Buffer.from(source);
-        (chunkOrAsset as any).source = Buffer.concat([prepend, sourceBuffer]);
-      }
-    },
-  };
-}
 
 export default defineConfig({
   build: {
@@ -47,7 +12,6 @@ export default defineConfig({
         ssr: false,
       }),
     tsconfigPaths(),
-    transformPdfJsWorker(),
   ],
   test: {
     coverage: {
