0ade1b9ebdd302992d93d3748ef4f2ab282e7915
Devin B <devin.c.blair@gmail.com>
2024-10-14 18:39:12 -0700

    style iconbutton. style resume layout and component dimensions.

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -2,39 +2,31 @@ import { memo } from 'react';
 import type { ComponentPropsWithoutRef } from 'react';
 import { classed } from '@tw-classed/react';
 
-interface ButtonProps
-  extends Pick<
-    ComponentPropsWithoutRef<'button'>,
-    'children' | 'className' | 'disabled' | 'onClick' | 'type'
-  > {
+interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   loading?: boolean;
   iconOnly?: boolean;
   variant?: 'primary' | 'secondary';
 }
 
-const StyledButton = classed(
-  'button',
-  'bg-slate-500 text-sky-100 font-medium',
-  {
-    variants: {
-      variant: {
-        primary: '',
-        secondary: '',
-      },
-      loading: {
-        true: '',
-        false: '',
-      },
-      iconOnly: {
-        true: 'rounded-full',
-        false: '',
-      },
+const StyledButton = classed('button', 'text-sky-100 font-medium', {
+  variants: {
+    variant: {
+      primary: 'bg-slate-500',
+      secondary: '',
     },
-    defaultVariants: {
-      variant: 'primary',
+    loading: {
+      true: '',
+      false: '',
+    },
+    iconOnly: {
+      true: 'p-3 size-8 md:size-16 flex items-center justify-center bg-slate-400 rounded-full hover:outline-2 hover:outline-sky-900 transition-all duration-1000 hover:bg-slate-400 focus:outline-2 focus:outline-sky-900',
+      false: '',
     },
   },
-);
+  defaultVariants: {
+    variant: 'primary',
+  },
+});
 
 const BaseButton = ({
   children,
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -17,10 +17,8 @@ const Resume = () => {
     setPageNumber((prevPageNumber) => {
       if (direction === 'forward' && prevPageNumber < 2) {
         return prevPageNumber + 1;
-      } else if (direction === 'back' && prevPageNumber > 1) {
-        return prevPageNumber - 1;
-      } else if (direction === 'back' && prevPageNumber < 2) {
-        return 2;
+      } else if (direction === 'back') {
+        return prevPageNumber > 1 ? prevPageNumber - 1 : 2;
       }
       return prevPageNumber - 1;
     });
@@ -28,12 +26,38 @@ const Resume = () => {
   return (
     <div className="grow-1 flex size-full flex-col items-center p-2 md:space-y-12 md:p-6">
       <Suspense fallback={null}>
-        <div className="flex">
-          <Button onClick={() => handlePageChange('back')}>Back</Button>
+        <div className="flex items-center space-x-4">
+          <Button
+            aria-label="page back"
+            iconOnly
+            onClick={() => handlePageChange('back')}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+              className="size-6 md:size-12"
+              viewBox="0 0 24 24"
+            >
+              <path d="M14.657 18.657a1 1 0 0 1-.707-.293l-5.657-5.657a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 0 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-.707 1.707z" />
+            </svg>
+          </Button>
           <Document file="assets/files/devin_blair_resume_fr.pdf">
             <Page pageNumber={pageNumber} />
           </Document>
-          <Button onClick={() => handlePageChange('forward')}>Forward</Button>
+          <Button
+            aria-label="navigate forward"
+            iconOnly
+            onClick={() => handlePageChange('forward')}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+              className="size-6 md:size-12"
+              viewBox="0 0 24 24"
+            >
+              <path d="M9.343 18.657a1 1 0 0 1-.707-1.707l4.95-4.95-4.95-4.95a1 1 0 0 1 1.414-1.414l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-.707.293z" />
+            </svg>
+          </Button>
         </div>
       </Suspense>
     </div>
