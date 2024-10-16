3e05587639940c6f162b9547fadc66fd43587db7
Devin B <devin.c.blair@gmail.com>
2024-10-16 00:52:19 -0700

    create resizeobserver. add observer to resume. further style resume.

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,4 +1,5 @@
-import { Suspense, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
+import { useResizeObserver } from '~/hooks/useResizeObserver';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { Button } from '~/components';
 // enables link annotations
@@ -13,6 +14,8 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 const Resume = () => {
   const [pageNumber, setPageNumber] = useState(1);
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const { width } = useResizeObserver({ ref: divRef });
 
   const handlePageChange = (direction: 'back' | 'forward') => {
     setPageNumber((prevPageNumber) => {
@@ -25,11 +28,12 @@ const Resume = () => {
     });
   };
   return (
-    <div className="grow-1 flex size-full flex-col items-center p-2 md:space-y-12 md:p-6">
+    <div className="grow-1 flex size-full flex-col items-center p-2 md:space-y-12 md:px-10 md:py-6">
       <Suspense fallback={null}>
-        <div className="flex items-center space-x-4">
+        <div className="flex w-full items-center justify-center space-x-4 px-4">
           <Button
             aria-label="page back"
+            className="hidden md:block"
             iconOnly
             onClick={() => handlePageChange('back')}
           >
@@ -42,11 +46,20 @@ const Resume = () => {
               <path d="M14.657 18.657a1 1 0 0 1-.707-.293l-5.657-5.657a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 0 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-.707 1.707z" />
             </svg>
           </Button>
-          <Document file="assets/files/devin_blair_resume_fr.pdf">
-            <Page pageNumber={pageNumber} />
-          </Document>
+          <div className="w-full md:w-7/12" ref={divRef}>
+            <Document file="assets/files/devin_blair_resume_fr.pdf">
+              <div className="hidden md:block">
+                <Page pageNumber={pageNumber} width={width} />
+              </div>
+              <div className="block space-y-2 md:hidden">
+                <Page pageNumber={1} width={width} />
+                <Page pageNumber={2} width={width} />
+              </div>
+            </Document>
+          </div>
           <Button
             aria-label="navigate forward"
+            className="hidden md:block"
             iconOnly
             onClick={() => handlePageChange('forward')}
           >
