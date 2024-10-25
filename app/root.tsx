990233448514f795ba7a74f51a3102e624a115ce
Devin B <devin.c.blair@gmail.com>
2024-10-24 18:32:48 -0700

    add overflow hidden to html.

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export const meta: MetaFunction = () => {
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="motion-safe:scroll-smooth" lang="en">
+    <html className="overflow-x-hidden motion-safe:scroll-smooth" lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
