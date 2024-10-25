6e4b805e6acb4fd3c0f5b79660593882d893f8c7
Devin B <devin.c.blair@gmail.com>
2024-10-24 17:32:16 -0700

    write stories for businesslogocard.

---
+++ app/components/BusinessLogoCard/BusinessLogoCard.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { BusinessLogoCard } from './BusinessLogoCard';
+import { IconComcastBusiness } from '../Icons';
+
+const meta = {
+  title: 'BusinessLogoCard',
+  component: BusinessLogoCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof BusinessLogoCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WithLink: Story = {
+  args: {
+    icon: <IconComcastBusiness />,
+    to: 'https://business.comcast.com',
+  },
+};
+
+export const WithoutHeader: Story = {
+  args: {
+    withLink: false,
+    icon: <p>no link here</p>,
+  },
+};
